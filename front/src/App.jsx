27099d7ebe06bf15529fa81125aea5cca38d9efb
aboud <abderrahmen.trabelsi@esprit.tn>
2024-03-28 10:59:17 +0100

    📝 chore(dataSources.xml): add dataSources.xml file for project configuration 🔧 fix(dataSources.xml): update JDBC URL in dataSources.xml to use relative path 🎨 style(App.css): add dark mode toggle button styles for better user experience 🚀 feat(App.jsx): implement dark mode functionality with toggle switch in App component

--- front/src/App.jsx
+++ front/src/App.jsx
@@ -1,26 +1,41 @@
-// eslint-disable-next-line no-unused-vars
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import TodosPage from './pages/TodosPage.jsx'; // Ensure correct path
+import TodosPage from './pages/TodosPage.jsx';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import TodoDetailsPage from "./pages/TodoDetailsPage.jsx";
 import 'bootstrap-icons/font/bootstrap-icons.css';
 
-
-
 function App() {
+    const [isDarkMode, setIsDarkMode] = useState(false);
+
+    const toggleDarkMode = () => {
+        setIsDarkMode(!isDarkMode);
+    };
+
+    useEffect(() => {
+        if (isDarkMode) {
+            document.documentElement.style.setProperty('--background-color', '#242424');
+            document.documentElement.style.setProperty('--text-color', '#ffffff');
+        } else {
+            document.documentElement.style.setProperty('--background-color', '#ffffff');
+            document.documentElement.style.setProperty('--text-color', '#000000');
+        }
+    }, [isDarkMode]);
+
     return (
         <Router>
-                <nav>
-                    <Link to="/">Home</Link> | <Link to="/todos">Todos</Link>
-                </nav>
-                <Routes>
-                    <Route path="/todos" element={<TodosPage />} />
-                    <Route path="/" element={<Home />} />
-                    <Route path="/todos/:todoId" element={<TodoDetailsPage />} />
-
-                </Routes>
+            <nav>
+                <div className={`toggle-button ${isDarkMode ? 'dark-mode' : ''}`} onClick={toggleDarkMode}>
+                    <div className="toggle-switch"></div>
+                </div>
+                <Link to="/">Home</Link> | <Link to="/todos">Todos</Link>
+            </nav>
+            <Routes>
+                <Route path="/todos" element={<TodosPage />} />
+                <Route path="/" element={<Home />} />
+                <Route path="/todos/:todoId" element={<TodoDetailsPage />} />
+            </Routes>
         </Router>
     );
 }
